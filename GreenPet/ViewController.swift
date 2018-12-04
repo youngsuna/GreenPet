b6f02b733904c8757340e4ac50e988a972e67ac3
woori <youngsu@wooriui-imac-4.local>
2018-12-04 10:06:08 +0900

    Initial Commit

---
+++ GreenPet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenPet
+//
+//  Created by woori on 04/12/2018.
+//  Copyright © 2018 youngsuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
